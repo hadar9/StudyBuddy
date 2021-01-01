c531a5b5eb2ee046e2bd0878b8c54c1957745734
hadar9 <hadar0934@gmail.com>
2021-01-02 01:52:22 +0200

    edited int initial report section 6 photos updated auth token expiration time edited requirement report added project video added presentation

--- program/routes/api/auth.js
+++ program/routes/api/auth.js
@@ -90,7 +90,7 @@ router.post(
         payload,
         config.get('jwtSecret'),
         {
-          expiresIn: 360000,
+          expiresIn: 3600,
         },
         (err, token) => {
           if (err) throw err;
@@ -145,7 +145,7 @@ router.post(
         payload,
         config.get('jwtSecret'),
         {
-          expiresIn: 360000,
+          expiresIn: 3600,
         },
         (err, token) => {
           if (err) throw err;
