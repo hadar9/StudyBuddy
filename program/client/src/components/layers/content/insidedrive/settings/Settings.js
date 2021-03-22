1b264cd1b76fdba8e2d04ad504c078d4a8b6db63
hadar9 <hadar0934@gmail.com>
2021-03-22 14:21:03 +0200

    drive setting private/public switch button

--- program/client/src/components/layers/content/insidedrive/settings/Settings.js
+++ program/client/src/components/layers/content/insidedrive/settings/Settings.js
@@ -1,5 +1,19 @@
 import React from 'react';
+import BootstrapSwitchButton from 'bootstrap-switch-button-react';
 
 export default function Settings() {
-  return <div></div>;
+  return (
+    <div className='settings'>
+      <h1 className='settings-title'>Settings</h1>
+      <BootstrapSwitchButton
+        checked={true}
+        onstyle='info'
+        offstyle='dark'
+        onlabel='public'
+        offlabel='private'
+        size='lg'
+        width='100'
+      />
+    </div>
+  );
 }
