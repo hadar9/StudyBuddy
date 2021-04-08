90b15213320a139ffa5a8987bd00f03ce9971356
hadar9 <hadar0934@gmail.com>
2021-04-08 14:48:46 +0300

    h

--- program/client/src/actions/drives.js
+++ program/client/src/actions/drives.js
@@ -37,6 +37,7 @@ export const searchdrives = (drivename) => async (dispatch) => {
         status: error.response.status,
       },
     });
+    console.log('dfdf');
   }
 };
 export const clearsearchdrives = () => async (dispatch) => {
