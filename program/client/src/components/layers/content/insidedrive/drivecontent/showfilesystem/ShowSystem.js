7f2a109ef867b9ba0f04cc24aae73ae4c5aba0fe
hadar9 <hadar0934@gmail.com>
2021-04-10 15:00:27 +0300

    s

--- program/client/src/components/layers/content/insidedrive/drivecontent/showfilesystem/ShowSystem.js
+++ program/client/src/components/layers/content/insidedrive/drivecontent/showfilesystem/ShowSystem.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Image, Col, Button } from 'react-bootstrap';
+import React, { useState } from 'react';
+import { Image, Col, Button, Modal, ModalBody } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.css';
 import fileimg from '../../../../../../img/file.png';
 import folderimg from '../../../../../../img/folder.png';
@@ -9,30 +9,48 @@ import PropTypes from 'prop-types';
 import FileViewer from 'react-file-viewer';
 
 function ShowSystem({ elem, choosefolder, choosefile }) {
+  const [showfile, setfile] = useState(false);
+
+  const showfilecontent = (e) => {
+    choosefile(elem);
+    setfile(true);
+  };
+
   return (
-    <div className='showfilesystem text-center'>
-      <Col>
-        {elem.objtype === 'folder' ? (
-          <Button variant='light' onClick={(e) => choosefolder(elem)}>
-            <Image className='folderimg' src={folderimg} />
-          </Button>
-        ) : (
-          <Button variant='light' onClick={(e) => choosefile(elem)}>
-            <Image className='fileimg' src={fileimg} />
-          </Button>
-        )}
-        <p
-          style={{
-            whiteSpace: 'nowrap',
-            color: 'black',
-            overflow: 'hidden',
-            textOverflow: 'ellipsis',
-            width: '150px',
-          }}
-        >
-          {elem.name}
-        </p>
-      </Col>
+    <div>
+      {showfile ? (
+        <div></div>
+      ) : (
+        <div className='showfilesystem text-center'>
+          <Col>
+            {elem.objtype === 'folder' ? (
+              <Button variant='light' onClick={(e) => choosefolder(elem)}>
+                <Image className='folderimg' src={folderimg} />
+              </Button>
+            ) : (
+              <Button
+                variant='light'
+                onClick={(e) => {
+                  showfilecontent(e);
+                }}
+              >
+                <Image className='fileimg' src={fileimg} />
+              </Button>
+            )}
+            <p
+              style={{
+                whiteSpace: 'nowrap',
+                color: 'black',
+                overflow: 'hidden',
+                textOverflow: 'ellipsis',
+                width: '150px',
+              }}
+            >
+              {elem.name}
+            </p>
+          </Col>
+        </div>
+      )}
     </div>
   );
 }
