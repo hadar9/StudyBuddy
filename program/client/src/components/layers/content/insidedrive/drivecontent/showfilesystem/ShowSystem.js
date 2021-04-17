c8d147074a3fea55ae0e79cf0d2936cd0ae150cf
hadar9 <hadar0934@gmail.com>
2021-04-17 19:54:48 +0300

    images types added

--- program/client/src/components/layers/content/insidedrive/drivecontent/showfilesystem/ShowSystem.js
+++ program/client/src/components/layers/content/insidedrive/drivecontent/showfilesystem/ShowSystem.js
@@ -1,13 +1,42 @@
 import React from 'react';
-import { Image, Col, Button, Modal } from 'react-bootstrap';
+import { Image, Col, Button } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.css';
-import fileimg from '../../../../../../img/file.png';
-import folderimg from '../../../../../../img/folder.png';
 import { choosefolder, choosefile } from '../../../../../../actions/filesystem';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
+/*import files pic*/
+import avi from '../../../../../../img/files/avi.png';
+import csv from '../../../../../../img/files/csv.png';
+import pdf from '../../../../../../img/files/pdf.png';
+import jpg from '../../../../../../img/files/jpg.png';
+import doc from '../../../../../../img/files/doc.png';
+import docx from '../../../../../../img/files/docx.png';
+import mp3 from '../../../../../../img/files/mp3.png';
+import mp4 from '../../../../../../img/files/mp4.png';
+import png from '../../../../../../img/files/png.png';
+import ppt from '../../../../../../img/files/ppt.png';
+import rar from '../../../../../../img/files/rar.png';
+import txt from '../../../../../../img/files/txt.png';
+import zip from '../../../../../../img/files/zip.png';
+import folderimg from '../../../../../../img/folder.png';
 
 function ShowSystem({ elem, choosefolder, choosefile }) {
+  const images = {
+    avi,
+    csv,
+    pdf,
+    jpg,
+    doc,
+    docx,
+    mp3,
+    mp4,
+    png,
+    ppt,
+    rar,
+    txt,
+    zip,
+  };
+
   const showfilecontent = (e) => {
     choosefile(elem);
     let url = elem.url;
@@ -32,7 +61,7 @@ function ShowSystem({ elem, choosefolder, choosefile }) {
                 showfilecontent(e);
               }}
             >
-              <Image className='fileimg' src={fileimg} />
+              <Image className='fileimg' src={images[`${elem.objtype}`]} />
             </Button>
           )}
           <p
