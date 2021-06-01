da9c73640fe6a99dbfca6d6e5ee87e70fda8a7e6
hadar9 <hadar0934@gmail.com>
2021-06-01 17:52:55 +0300

    check

--- program/server.js
+++ program/server.js
@@ -4,22 +4,27 @@ const connectDB = require('./config/db');
 const app = express();
 const server = http.createServer(app);
 const cors = require('cors');
-
+const path = require('path');
 //Connect to DB
 connectDB();
 
 //Init Middleware
 app.use(express.json({ extended: false }));
 app.use(cors());
-app.get('/', (req, res) => res.send('API Running'));
 
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/buddies', require('./routes/api/buddies'));
 app.use('/api/drives', require('./routes/api/drives'));
 app.use('/api/filesystem', require('./routes/api/filesystem'));
-app.use('/api/chat', require('./routes/api/chat'))
+app.use('/api/chat', require('./routes/api/chat'));
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 const PORT = process.env.PORT || 5000;
 
 server.listen(PORT, () => console.log(`Server started on PORT ${PORT}...`));
